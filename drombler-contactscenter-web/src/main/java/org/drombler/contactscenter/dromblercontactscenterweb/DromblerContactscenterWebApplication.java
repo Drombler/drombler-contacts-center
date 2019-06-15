55f641cefbb12a2cca7b02e74811f79766ce8648
Florian Brunner <fbrunnerreg@gmx.ch>
2019-06-15 12:04:53 +0200

    initial import

---
+++ drombler-contactscenter-web/src/main/java/org/drombler/contactscenter/dromblercontactscenterweb/DromblerContactscenterWebApplication.java
@@ -0,0 +1,13 @@
+package org.drombler.contactscenter.dromblercontactscenterweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DromblerContactscenterWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DromblerContactscenterWebApplication.class, args);
+	}
+
+}
---
+++ drombler-contactscenter-web/src/test/java/org/drombler/contactscenter/dromblercontactscenterweb/DromblerContactscenterWebApplicationTests.java
@@ -0,0 +1,16 @@
+package org.drombler.contactscenter.dromblercontactscenterweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DromblerContactscenterWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
